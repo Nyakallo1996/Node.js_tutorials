68f593e20d8a33c0a032065dce14957b5c29efec
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-06 22:01:39 +0200

    node.js

---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = "super secret"
+// share
+const john = "john"
+const peter = "peter"
+
+// exports default for more than one variable
+module.exports = {john, peter}//object
